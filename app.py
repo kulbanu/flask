87f7436e4a53016d0891627020c8ef192377f5ff
Kulbanu <r.r.r.r.4.g.a@gmail.com>
2024-03-29 14:10:39 +0500

    fix

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def answer():
     prompt = request.json['prompt']
     print(prompt);
     response = g4f.ChatCompletion.create(
-    model=g4f.models.gpt_35_turbo_16k_0613,
+    model=g4f.models.gpt_4,
     messages=[{"role": "user", "content": prompt}])
     return jsonify({'response': response})
 
